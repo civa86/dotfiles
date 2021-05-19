3ee75648879660745b9e25956a294507bca7f36a
Dario Civallero <dario.civallero@gmail.com>
2021-05-19 12:28:33 +0200

    update

--- utils/path.sh
+++ utils/path.sh
@@ -1,3 +1,6 @@
 # PATH
 #export PATH="$DOTFILES/scripts/bin:$HOME/.gem/ruby/2.3.0/bin:$HOME/.rbenv/bin:/Applications/MySQLWorkbench.app/Contents/MacOS:$PATH"
 export PATH="$DOTFILES/scripts/bin:$HOME/.gem/ruby/2.3.0/bin:$HOME/.rbenv/bin:$PATH"
+if [ -d $HOME/.poetry/bin ]; then
+    export PATH="$HOME/.poetry/bin:$PATH"
+fi
