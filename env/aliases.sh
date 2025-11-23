65fa194e785251af67e0e6222d0cfb2dc4c989e3
Dario Civallero <dario.civallero@gmail.com>
2025-11-23 11:40:18 +0100

    update alias

--- env/aliases.sh
+++ env/aliases.sh
@@ -14,3 +14,6 @@ alias mysql-client='docker run -it mysql:5.7 mysql'
 # MongoDB
 alias mongo-start='docker run -d --name mongo-local -p 27017:27017 mongo:4.0'
 alias mongo-stop='docker stop mongo-local && docker rm mongo-local'
+
+# Custom Software
+alias mtbwise='~/Workspace/civa86/mtb-wise/run.sh'
